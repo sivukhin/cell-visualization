6a8def1cbd9641361645d859f9fe70d3dc9c0472
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2021-11-23 19:07:17 +0500

    persist state in local storage

--- src/world/world.ts
+++ src/world/world.ts
@@ -1,10 +1,10 @@
-import {Unwrap, WorldConfiguration} from "../configuration";
-import {Object3D, Vector2} from "three";
-import {getComponents} from "../utils/geometry";
-import {createAliveCell} from "./cell";
-import {CellElement, CellInfo, WorldElement} from "./types";
-import {to2} from "../utils/draw";
-import {interpolateMany, randomFrom} from "../utils/math";
+import { Unwrap, WorldConfiguration } from "../configuration";
+import { Object3D, Vector2 } from "three";
+import { getComponents } from "../utils/geometry";
+import { createAliveCell } from "./cell";
+import { CellElement, CellInfo, WorldElement } from "./types";
+import { to2 } from "../utils/draw";
+import { interpolateMany, randomFrom } from "../utils/math";
 
 interface CellState {
     velocity: Vector2;
@@ -21,6 +21,19 @@ function occupy<T>(occupied: T[][], p: T, row: number, col: number, rows: number
     occupied[row * cols + Math.max(0, col - 1)].push(p);
 }
 
+const urlSearchParams = new URLSearchParams(window.location.search);
+if (urlSearchParams.has("clear")) {
+    console.info("clear local storage");
+    localStorage.clear();
+}
+
+const cellPositionsRaw = localStorage.getItem("cells-positions");
+let initialPositions = new Map<number, Vector2>();
+if (cellPositionsRaw != null) {
+    initialPositions = new Map<number, Vector2>(JSON.parse(cellPositionsRaw).map((x) => [x[0], new Vector2(x[1], x[2])]));
+    console.info("state was loaded: ", cellPositionsRaw, initialPositions);
+}
+
 export function createWorld(worldConfig: Unwrap<WorldConfiguration>): WorldElement {
     const multiverse = new Object3D();
 
@@ -28,6 +41,16 @@ export function createWorld(worldConfig: Unwrap<WorldConfiguration>): WorldEleme
 
     const vPadding = 150;
     const hPadding = 50;
+
+    setInterval(() => {
+        const currentPositions = [];
+        for (const entry of cells.entries()) {
+            const position = entry[1].element.multiverse.position;
+            currentPositions.push([entry[0], position.x, position.y]);
+        }
+        localStorage.setItem("cells-positions", JSON.stringify(currentPositions));
+        console.info("state was persisted: ", currentPositions);
+    }, 1000);
     return {
         multiverse: multiverse,
         getOrganells(cellId: number, organells: number[]): Vector2[] {
@@ -106,28 +129,37 @@ export function createWorld(worldConfig: Unwrap<WorldConfiguration>): WorldEleme
                 } else {
                     const cell = createAliveCell(worldConfig.cell, worldConfig.flagellum);
                     const velocity = new Vector2(worldConfig.speed, 0);
-                    let slot = Math.min(occupied.length - 1, Math.floor(randomFrom(0, occupied.length)));
-                    let iteration = 0;
-                    while (true) {
-                        const center = new Vector2(
-                            col * (slot % cols) - worldConfig.soup.width / 2 + vPadding + col / 2,
-                            row * Math.floor(slot / cols) - worldConfig.soup.height / 2 + hPadding + row / 2
+                    let position = null;
+                    if (initialPositions.has(cellInfo.id)) {
+                        position = initialPositions.get(cellInfo.id);
+                    } else {
+                        let slot = Math.min(occupied.length - 1, Math.floor(randomFrom(0, occupied.length)));
+                        let iteration = 0;
+                        while (true) {
+                            const center = new Vector2(
+                                col * (slot % cols) - worldConfig.soup.width / 2 + vPadding + col / 2,
+                                row * Math.floor(slot / cols) - worldConfig.soup.height / 2 + hPadding + row / 2
+                            );
+                            let minDistance = Infinity;
+                            for (const [p, size] of occupied[slot]) {
+                                minDistance = Math.min(minDistance, center.distanceTo(p) - size - sizes[i]);
+                            }
+                            if (minDistance > -iteration) {
+                                break;
+                            }
+                            iteration++;
+                            slot = (slot + 1) % occupied.length;
+                        }
+                        position = new Vector2(
+                            col * (slot % cols) - worldConfig.soup.width / 2 + vPadding + randomFrom(col / 3, (2 * col) / 3),
+                            row * Math.floor(slot / cols) - worldConfig.soup.height / 2 + hPadding + randomFrom(row / 3, (2 * row) / 3)
                         );
-                        let minDistance = Infinity;
-                        for (const [p, size] of occupied[slot]) {
-                            minDistance = Math.min(minDistance, center.distanceTo(p) - size - sizes[i]);
-                        }
-                        if (minDistance > -iteration) {
-                            break;
-                        }
-                        iteration++;
-                        slot = (slot + 1) % occupied.length;
                     }
-                    const position = new Vector2(
-                        col * (slot % cols) - worldConfig.soup.width / 2 + vPadding + randomFrom(col / 3, (2 * col) / 3),
-                        row * Math.floor(slot / cols) - worldConfig.soup.height / 2 + hPadding + randomFrom(row / 3, (2 * row) / 3)
-                    );
-                    occupy(occupied, [position, sizes[i]], Math.floor(slot / cols), slot % cols, rows, cols);
+
+                    const currentRow = Math.min(rows - 1, Math.floor((position.y + worldConfig.soup.height / 2 - hPadding) / row));
+                    const currentCol = Math.min(cols - 1, Math.floor((position.x + worldConfig.soup.width / 2 - vPadding) / col));
+                    occupy(occupied, [position, sizes[i]], currentRow, currentCol, rows, cols);
+
                     position.x = Math.min(worldConfig.soup.width - sizes[i] - vPadding, Math.max(-worldConfig.soup.width + sizes[i] + vPadding, position.x));
                     position.y = Math.min(worldConfig.soup.height - sizes[i] - hPadding, Math.max(-worldConfig.soup.height + sizes[i] + hPadding, position.y));
                     cell.multiverse.position.set(position.x, position.y, 0);
